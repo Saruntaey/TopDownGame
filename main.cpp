e43faf71381d1d75e4a767fdb80e3c70ba0b5241
Sarun Tapee <tpzrun@gmail.com>
2021-08-30 06:09:17 +0700

    loop the game

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 #include "Hero.h"
 #include "Enemy.h"
 
+bool reqWindowClose {false};
+bool isFirstOpen {true};
+
+void gameIntro(int windowWidth, int windowHeight);
+void gameLogic(int windowWidth, int windowHeight);
+void gameOver();
+
 int main()
 {
     const int windowWidth {384 * 2};
@@ -11,6 +18,94 @@ int main()
     InitWindow(windowWidth, windowHeight, "Top down game");
     SetTargetFPS(60);
 
+    
+
+    while (!reqWindowClose)
+    {
+        gameIntro(windowWidth, windowHeight);
+        gameLogic(windowHeight, windowHeight);
+        gameOver();
+    }
+    CloseWindow();
+}
+
+void gameIntro(int windowWidth, int windowHeight)
+{
+    if (!isFirstOpen) return;
+    // load map
+    Texture2D map {LoadTexture("nature_tileset/OpenWorldMap.png")};
+    float mapScale {4.f};
+    Vector2 mapPos {-map.width/2.f * mapScale, 0.f};
+   
+
+    // load knight
+    Texture2D knight {LoadTexture("characters/Knight/Run.png")};
+    float scale {4.f};
+    
+    int frame {};
+    int frameMax {5};
+    float width {static_cast<float>(knight.width)/(frameMax + 1)};
+    float height {static_cast<float>(knight.height)};
+    float updatingTime {1.f/12.f};
+    float runningTime {};
+    // 1 facing right, -1 facing left
+    float rightLeft {1.f};
+    float speed {160.f};
+    float offset {50.f};
+
+    float leftBound {offset};
+    float rightBound {windowWidth - (offset + width * scale)};
+    float x_pos {leftBound};
+    while (true)
+    {
+        if (WindowShouldClose())
+        {   
+            reqWindowClose = true;
+            break;
+        }
+        if (IsKeyDown(KEY_SPACE))
+        {
+            isFirstOpen = false;
+            break;
+        } 
+        
+        BeginDrawing();
+        ClearBackground(WHITE);
+        // draw map
+        DrawTextureEx(map, mapPos, 0.f, mapScale, WHITE);
+        // draw knight
+        float dT = GetFrameTime();
+        runningTime += dT;
+        x_pos += speed * dT;
+        if (x_pos < leftBound || x_pos > rightBound)
+        {
+            speed *= -1;
+        }
+        rightLeft = speed > 0 ? 1.f : -1.f;
+        if (runningTime >= updatingTime)
+        {
+            runningTime = 0;
+            frame++;
+            if (frame > frameMax) frame = 0;
+        }
+        Rectangle src {frame * width, 0.f, rightLeft * width, height};
+        Rectangle dest {x_pos, windowHeight - (scale * height + offset), scale * width, scale * height};
+        DrawTexturePro(knight, src, dest, Vector2{}, 0.f, WHITE);
+        DrawText("Welcome to Top Down Game!", 55.f, 45.f, 40, RED);
+        DrawText("Control: A, S, W, D.", 55.f, 100.f, 30, RED);
+        DrawText("Attack: Left click.", 55.f, 150.f, 30, RED);
+        DrawText("Press spacebar to start.", 55.f, 200.f, 30, RED);
+        EndDrawing();
+    }
+    UnloadTexture(map);
+    UnloadTexture(knight);
+    
+
+}
+
+void gameLogic(int windowWidth, int windowHeight)
+{
+    if (reqWindowClose) return;
     // load map
     Texture2D map {LoadTexture("nature_tileset/OpenWorldMap.png")};
     Vector2 mapPos {};
@@ -39,20 +134,18 @@ int main()
         &sk
     };
 
-    while (!WindowShouldClose())
+    while (kn.getAlive())
     {
+        if (WindowShouldClose())
+        {
+            reqWindowClose = true;
+            break;
+        } 
         BeginDrawing();
         ClearBackground(WHITE);
         // draw map
         mapPos = Vector2Scale(kn.getWorldPos(), -1.f);
         DrawTextureEx(map, mapPos, 0.f, mapScale, WHITE);
-
-        if (!kn.getAlive())
-        {
-            DrawText("Game Over!", 55.f, 45.f, 40, RED);
-            EndDrawing();
-            continue;
-        }
 
         print_health(&kn);
 
@@ -86,10 +179,65 @@ int main()
         }
         // Draw mimimap
         plotMiniMap(&kn, enemies, 3, &map, mapScale, windowWidth, windowHeight);
+        
+        // check is all enemy was destroy
+        bool isAllEnemyDie {true};
+        for (auto enemy: enemies)
+        {
+            if (enemy->getAlive())
+            {
+                isAllEnemyDie = false;
+                break;
+            }
+        }
+        if (isAllEnemyDie)
+        {
+            DrawText("Victory", 55.f, 100.f, 30, RED);
+            DrawText("Press spacebar to play again.", 55.f, 150.f, 30, RED);
+            if (IsKeyDown(KEY_SPACE))
+            {
+                break;
+            }
+        }
+
         EndDrawing();
     }
     UnloadTexture(sword);
     UnloadCharacter(goblin, mushroom, skeleton, knight);
     UnloadTexture(map);
-    CloseWindow();
+}
+
+void gameOver()
+{
+    if (reqWindowClose) return;
+    Texture bg {LoadTexture("nature_tileset/Combined background - Blue.png")};
+    float bg_x {};
+    while (true)
+    {
+        if (IsKeyDown(KEY_SPACE)) break;
+        if (WindowShouldClose())
+        {
+            reqWindowClose = true;
+            break;
+        }
+              
+        BeginDrawing();
+        ClearBackground(WHITE);
+        const float dT = GetFrameTime();
+        bg_x += -40 * dT;
+        // draw background
+        if (bg_x < -bg.width *2)
+        {
+            bg_x = 0;
+        }
+        float bg_scale {0.8f};
+        Vector2 bg1Pos {bg_x, 0};
+        DrawTextureEx(bg, bg1Pos, 0.f, bg_scale, WHITE);
+        Vector2 bg2Pos {bg_x + bg.width * bg_scale, 0};
+        DrawTextureEx(bg, bg2Pos, 0.f, 2.f, WHITE);
+        DrawText("Game Over!", 55.f, 45.f, 40, RED);
+        DrawText("Press spacebar to play again.", 55.f, 100.f, 30, RED);
+        EndDrawing();
+    }
+    UnloadTexture(bg);
 }
